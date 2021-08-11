ef67276cd4c846e6238a84f374071ecb154b5e49
Victor Onofiok <radoncreep@gmail.com>
2021-08-11 22:44:38 +0100

    brute logic

--- gui-tictactoe.py
+++ gui-tictactoe.py
@@ -2,11 +2,91 @@ from tkinter import *
 from tkinter import messagebox
 
 root = Tk()
-root.title('Tic-Tac-Toe-blah-blah')
+root.title('Tic-Tac-Toe-CS514-AI')
 # root.geometry("480x480")
 
 clicked = True
 count = 0
+
+
+def start():
+    global b1, b2, b3, b4, b5, b6, b7, b8, b9
+    # we need 9 buttons for the tictactoe grid
+    b1 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b1))
+    b2 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b2))
+    b3 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b3))
+
+    b4 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b4))
+    b5 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b5))
+    b6 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b6))
+
+    b7 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b7))
+    b8 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b8))
+    b9 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b9))
+
+    # Grid buttons to screen
+    b1.grid(row=0, column=0)
+    b2.grid(row=0, column=1)
+    b3.grid(row=0, column=2)
+
+    b4.grid(row=1, column=0)
+    b5.grid(row=1, column=1)
+    b6.grid(row=1, column=2)
+
+    b7.grid(row=2, column=0)
+    b8.grid(row=2, column=1)
+    b9.grid(row=2, column=2)
+
+
+def restart():
+    global b1, b2, b3, b4, b5, b6, b7, b8, b9
+
+    global clicked, count
+    clicked = True
+    count = 0
+
+    b1 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b1))
+    b2 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b2))
+    b3 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b3))
+
+    b4 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b4))
+    b5 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b5))
+    b6 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b6))
+
+    b7 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b7))
+    b8 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b8))
+    b9 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: btn_click(b9))
+
+    # Grid buttons to screen
+    b1.grid(row=0, column=0)
+    b2.grid(row=0, column=1)
+    b3.grid(row=0, column=2)
+
+    b4.grid(row=1, column=0)
+    b5.grid(row=1, column=1)
+    b6.grid(row=1, column=2)
+
+    b7.grid(row=2, column=0)
+    b8.grid(row=2, column=1)
+    b9.grid(row=2, column=2)
 
 
 def disable_all_buttons():
@@ -31,56 +111,56 @@ def check_for_win():
         b3.config(bg="green")
         winner = True
         messagebox.showinfo('Tic-Tac-Toe', "Player X wins")
-        disable_all_buttons
+        disable_all_buttons()
     elif b4["text"] == "X" and b5["text"] == "X" and b6["text"] == "X":
         b4.config(bg="green")
         b5.config(bg="green")
         b6.config(bg="green")
         winner = True
         messagebox.showinfo('Tic-Tac-Toe', "Player X wins")
-        disable_all_buttons
+        disable_all_buttons()
     elif b7["text"] == "X" and b8["text"] == "X" and b9["text"] == "X":
         b7.config(bg="green")
         b8.config(bg="green")
         b9.config(bg="green")
         winner = True
         messagebox.showinfo('Tic-Tac-Toe', "Player X wins")
-        disable_all_buttons
+        disable_all_buttons()
     elif b1["text"] == "X" and b4["text"] == "X" and b7["text"] == "X":
         b1.config(bg="green")
         b4.config(bg="green")
         b7.config(bg="green")
         winner = True
         messagebox.showinfo('Tic-Tac-Toe', "Player X wins")
-        disable_all_buttons
+        disable_all_buttons()
     elif b2["text"] == "X" and b5["text"] == "X" and b8["text"] == "X":
         b2.config(bg="green")
         b5.config(bg="green")
         b8.config(bg="green")
         winner = True
         messagebox.showinfo('Tic-Tac-Toe', "Player X wins")
-        disable_all_buttons
+        disable_all_buttons()
     elif b3["text"] == "X" and b6["text"] == "X" and b9["text"] == "X":
         b3.config(bg="green")
         b6.config(bg="green")
         b9.config(bg="green")
         winner = True
         messagebox.showinfo('Tic-Tac-Toe', "Player X wins")
-        disable_all_buttons
+        disable_all_buttons()
     elif b1["text"] == "X" and b5["text"] == "X" and b9["text"] == "X":
         b1.config(bg="green")
         b5.config(bg="green")
         b9.config(bg="green")
         winner = True
         messagebox.showinfo('Tic-Tac-Toe', "Player X wins")
-        disable_all_buttons
+        disable_all_buttons()
     elif b3["text"] == "X" and b5["text"] == "X" and b7["text"] == "X":
         b3.config(bg="green")
         b5.config(bg="green")
         b7.config(bg="green")
         winner = True
         messagebox.showinfo('Tic-Tac-Toe', "Player X wins")
-        disable_all_buttons
+        disable_all_buttons()
 
     #   for Player O
     elif b1["text"] == "O" and b2["text"] == "O" and b3["text"] == "O":
@@ -89,56 +169,60 @@ def check_for_win():
         b3.config(bg="green")
         winner = True
         messagebox.showinfo('Tic-Tac-Toe', "Player O wins")
-        disable_all_buttons
+        disable_all_buttons()
     elif b4["text"] == "O" and b5["text"] == "O" and b6["text"] == "O":
         b4.config(bg="green")
         b5.config(bg="green")
         b6.config(bg="green")
         winner = True
         messagebox.showinfo('Tic-Tac-Toe', "Player O wins")
-        disable_all_buttons
+        disable_all_buttons()
     elif b7["text"] == "O" and b8["text"] == "O" and b9["text"] == "O":
         b7.config(bg="green")
         b8.config(bg="green")
         b9.config(bg="green")
         winner = True
         messagebox.showinfo('Tic-Tac-Toe', "Player O wins")
-        disable_all_buttons
+        disable_all_buttons()
     elif b1["text"] == "O" and b4["text"] == "O" and b7["text"] == "O":
         b1.config(bg="green")
         b4.config(bg="green")
         b7.config(bg="green")
         winner = True
         messagebox.showinfo('Tic-Tac-Toe', "Player O wins")
-        disable_all_buttons
+        disable_all_buttons()
     elif b2["text"] == "O" and b5["text"] == "O" and b8["text"] == "O":
         b2.config(bg="green")
         b5.config(bg="green")
         b8.config(bg="green")
         winner = True
         messagebox.showinfo('Tic-Tac-Toe', "Player O wins")
-        disable_all_buttons
+        disable_all_buttons()
     elif b3["text"] == "O" and b6["text"] == "O" and b9["text"] == "O":
         b3.config(bg="green")
         b6.config(bg="green")
         b9.config(bg="green")
         winner = True
         messagebox.showinfo('Tic-Tac-Toe', "Player O wins")
-        disable_all_buttons
+        disable_all_buttons()
     elif b1["text"] == "O" and b5["text"] == "O" and b9["text"] == "O":
         b1.config(bg="green")
         b5.config(bg="green")
         b9.config(bg="green")
         winner = True
         messagebox.showinfo('Tic-Tac-Toe', "Player O wins")
-        disable_all_buttons
+        disable_all_buttons()
     elif b3["text"] == "O" and b5["text"] == "O" and b7["text"] == "O":
         b3.config(bg="green")
         b5.config(bg="green")
         b7.config(bg="green")
         winner = True
         messagebox.showinfo('Tic-Tac-Toe', "Player O wins")
-        disable_all_buttons
+        disable_all_buttons()
+
+    if count == 9 and winner == False:
+        messagebox.showinfo("TicTacToe", "This round is a tie!")
+        disable_all_buttons()
 
 
 # btn is a widget
@@ -160,39 +244,12 @@ def btn_click(btn):
         messagebox.showerror("Tic-Tac-Toe", "Move made on this box already")
 
 
-# we need 9 buttons for the tictactoe grid
-b1 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: btn_click(b1))
-b2 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: btn_click(b2))
-b3 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: btn_click(b3))
+game_menu = Menu(root)
+root.config(menu=game_menu)
 
-b4 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: btn_click(b4))
-b5 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: btn_click(b5))
-b6 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: btn_click(b6))
+menu_options = Menu(game_menu, tearoff=False)
+game_menu.add_cascade(label="Options", menu=menu_options)
+menu_options.add_command(label="Restart Game", command=restart)
 
-b7 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: btn_click(b7))
-b8 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: btn_click(b8))
-b9 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-            command=lambda: btn_click(b9))
-
-# Grid buttons to screen
-b1.grid(row=0, column=0)
-b2.grid(row=0, column=1)
-b3.grid(row=0, column=2)
-
-b4.grid(row=1, column=0)
-b5.grid(row=1, column=1)
-b6.grid(row=1, column=2)
-
-b7.grid(row=2, column=0)
-b8.grid(row=2, column=1)
-b9.grid(row=2, column=2)
-
+start()
 root.mainloop()
